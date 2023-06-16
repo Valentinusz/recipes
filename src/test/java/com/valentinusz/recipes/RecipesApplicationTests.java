cd76310cb04087d4fbf4d4218c4bbb6fdeeed23e
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-06-16 19:08:49 +0200

    add body to contextLoads test case

--- src/test/java/com/valentinusz/recipes/RecipesApplicationTests.java
+++ src/test/java/com/valentinusz/recipes/RecipesApplicationTests.java
@@ -1,13 +1,19 @@
 package com.valentinusz.recipes;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class RecipesApplicationTests {
+	@Autowired
+    private ApplicationContext context;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(context);
 	}
-
 }

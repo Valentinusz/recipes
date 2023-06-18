112bf5792e889934a1be06c2ff1729aa89397600
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-06-18 14:00:21 +0200

    add test graphql resolver

--- src/main/java/com/valentinusz/recipes/resolvers/RootResolver.java
+++ src/main/java/com/valentinusz/recipes/resolvers/RootResolver.java
@@ -1,6 +1,6 @@
 package com.valentinusz.recipes.resolvers;
 
-import com.valentinusz.recipes.model.User;
+import com.valentinusz.recipes.models.User;
 import com.valentinusz.recipes.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;

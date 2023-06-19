b772edc92750ead779dd91317a764df33e518dfe
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-06-19 23:24:27 +0200

    create migration

---
+++ src/main/resources/database/migrations/V2023_06_19_201500__create_recipes_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE recipes (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(64) CHECK ( LENGTH(name) BETWEEN 8 AND 64) NOT NULL,
+    preparation_time INTEGER CHECK ( preparation_time > 0 ) NOT NULL,
+    cooking_time INTEGER CHECK ( cooking_time > 0 ) NOT NULL,
+    instructions TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+    thumbnail VARCHAR(64),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE
+)
+

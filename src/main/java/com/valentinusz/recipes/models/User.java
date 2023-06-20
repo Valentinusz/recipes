5adc989addf5265cbf7609102e014b6197d33c8e
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-06-20 20:46:51 +0200

    improve validation rules, add documentation for fields

--- src/main/java/com/valentinusz/recipes/models/User.java
+++ src/main/java/com/valentinusz/recipes/models/User.java
@@ -12,47 +12,62 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.Instant;
 import java.util.Collection;
 
+/** Entity representing a user. */
 @Entity
 @Table(name = "users")
 public class User {
+    /** Id of the user. */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    /** Username of the user. */
     @Column(unique = true, nullable = false, length = 20)
     @Size(min = 6, max = 20)
+    @NotNull
     private String userName;
 
+    /** Email address of the user. */
     @Column(unique = true, nullable = false, length = 32)
     @Email
+    @NotNull
     private String email;
 
+    /** Time at which the users email was verified at. */
+    @Column
     private Instant emailVerifiedAt;
 
-    @Column(unique = true, nullable = false, length = 20)
+    /** Password of the user. **/
+    @Column(unique = true, nullable = false, length = 60)
     @JsonIgnore
+    @NotNull
     private String password;
 
+    /** Holds whether the user is an admin or not. */
     @Column(nullable = false, columnDefinition = "bit")
     @ColumnDefault("0")
     private Boolean isAdmin;
 
+    /** Time at which the model was inserted into the database at. */
     @CreationTimestamp
     private Instant createdAt;
 
+    /** Time at which the model was last updated at. */
     @UpdateTimestamp
     private Instant updatedAt;
 
     @OneToMany(mappedBy = "author")
     private Collection<Recipe> recipes;
 
-    public User() {}
-
     public User(@NotNull String userName, @NotNull String email, @NotNull String password, @NotNull Boolean isAdmin) {
         this.userName = userName;
         this.email = email;
         this.password = password;
         this.isAdmin = isAdmin;
+    }
+
+    public User() {
+
     }
 
     public Integer getId() {

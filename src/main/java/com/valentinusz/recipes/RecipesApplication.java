b425f56010f1cd13c58d0e53487b14b9b1884596
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-06-20 20:46:07 +0200

    add bean production for dev profile

--- src/main/java/com/valentinusz/recipes/RecipesApplication.java
+++ src/main/java/com/valentinusz/recipes/RecipesApplication.java
@@ -1,10 +1,26 @@
 package com.valentinusz.recipes;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class RecipesApplication {
+	@Bean
+    @Profile("development")
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+    @Profile("development")
+	public Faker faker() {
+		return new Faker();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(RecipesApplication.class, args);
 	}

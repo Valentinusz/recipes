0612f5ebe940bd3673dc69db339424a7334d39eb
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-06-20 20:44:23 +0200

    increase password field size

--- src/main/resources/database/migrations/V2023_06_16_000000__create_users_table.sql
+++ src/main/resources/database/migrations/V2023_06_16_000000__create_users_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     user_name VARCHAR(20) CHECK ( LENGTH(user_name) BETWEEN 6 AND 20 ) NOT NULL,
     email VARCHAR(32) CHECK ( email LIKE '_%@_%.__%') UNIQUE NOT NULL,
     email_verified_at TIMESTAMP,
-    password VARCHAR(20) NOT NULL,
+    password VARCHAR(60) NOT NULL,
     is_admin BIT DEFAULT 0 NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL

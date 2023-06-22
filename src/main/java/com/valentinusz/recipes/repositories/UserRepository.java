0b227e36f54da294632628d6995ac025536b3779
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-06-22 19:26:07 +0200

    add query
    add query for finding user entities and also eagerly loading their dietary restrictions

--- src/main/java/com/valentinusz/recipes/repositories/UserRepository.java
+++ src/main/java/com/valentinusz/recipes/repositories/UserRepository.java
@@ -1,7 +1,19 @@
 package com.valentinusz.recipes.repositories;
 
 import com.valentinusz.recipes.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
+/** Repository for the user entity. */
 public interface UserRepository extends CrudRepository<User, Long> {
+    /**
+     * Finds the given user and loads their dietary restrictions.
+     * @param id id of the user to fetch information of.
+     * @return Optional instance.
+     */
+    @Query("SELECT u FROM User u JOIN FETCH u.dietaryRestrictions WHERE u.id = :id")
+    Optional<User> findUserByIdWithDietaryRestrictions(@Param("id") Long id);
 }

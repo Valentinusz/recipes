4193b78de2455e87f25e9e850fd9ba3fa3b5cad8
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-06-22 19:21:52 +0200

    create repository for entity

---
+++ src/main/java/com/valentinusz/recipes/repositories/DietaryRestrictionRepository.java
@@ -0,0 +1,7 @@
+package com.valentinusz.recipes.repositories;
+
+import com.valentinusz.recipes.models.DietaryRestriction;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DietaryRestrictionRepository extends CrudRepository<DietaryRestriction, Integer> {
+}

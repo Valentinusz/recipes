55122a258f248e33ebf878b6eed737607ae42e41
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-06-22 19:29:01 +0200

    add basic security configuration

---
+++ src/main/java/com/valentinusz/recipes/controllers/security/UserSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.valentinusz.recipes.controllers.security;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class UserSecurityConfig {
+    @Bean
+    public SecurityFilterChain userSecurityFilterChain(@NotNull HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.securityMatcher("/users").authorizeHttpRequests(authorize -> {
+            authorize.requestMatchers(HttpMethod.GET, "/users/{id}").permitAll();
+            authorize.requestMatchers(HttpMethod.GET, "/users/{id}/restrictions").permitAll();
+        });
+        return httpSecurity.build();
+    }
+}

ec02c903527df63b805266983e3e88948508d6a2
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-06-23 23:39:48 +0200

    capitalize values in check

--- src/main/resources/database/migrations/V2023_06_22_200100__create_ingredients_table.sql
+++ src/main/resources/database/migrations/V2023_06_22_200100__create_ingredients_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE ingredients (
     id INTEGER PRIMARY KEY,
     name VARCHAR(64) CHECK ( LENGTH(name) BETWEEN 2 AND 64) NOT NULL,
     energy INTEGER CHECK ( energy > 0 ) NOT NULL,
-    category VARCHAR(10) CHECK ( category IN ('fruit', 'vegetable', 'grain', 'meat', 'dairy', 'spice', 'miscellaneous')),
+    category VARCHAR(10) CHECK ( category IN ('FRUIT', 'VEGETABLE', 'GRAIN', 'MEAT', 'DAIRY', 'SPICE', 'MISCELLANEOUS')),
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 )

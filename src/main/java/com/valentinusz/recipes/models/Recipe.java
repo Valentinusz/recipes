bc6a3318ed723b9a24118eaab08ea9e9d2a4d08e
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-06-23 23:43:02 +0200

    improve entity, add relation to PortionedIngredient

--- src/main/java/com/valentinusz/recipes/models/Recipe.java
+++ src/main/java/com/valentinusz/recipes/models/Recipe.java
@@ -4,48 +4,65 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
 
 import java.time.Instant;
+import java.util.Collection;
 
 /** Entity representing a recipe. */
 @Entity
 @Table(name = "recipes")
+@Getter
+@Setter
 public class Recipe {
     /** Id of the recipe. */
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     /** Name of the recipe. */
-    @Size(min = 8, max = 64)
+    @Column(nullable = false)
+    @Size(min = 2, max = 64)
     @NotNull
     private String name;
 
     /** Time it takes to prepare ingredients for the recipe. */
+    @Column(nullable = false)
     @Min(0)
     @NotNull
     private Integer preparationTime;
 
     /** Time it takes to cook the given meal in minutes. */
+    @Column(nullable = false)
     @Min(0)
     @NotNull
     private Integer cookingTime;
 
-    // TODO: 2023. 06. 19. consider creating a dedicated object for this instead of using string.
+    // TODO: 2023. 06. 19. consider creating a dedicated object / table for this instead of using string.
     /** Instructions of the recipe. */
+    @Column(nullable = false)
+    @Length(min = 32)
     @NotNull
     private String instructions;
 
     /** Author of the recipe. */
-    @ManyToOne(optional = false, cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    @ManyToOne(optional = false, cascade = CascadeType.REMOVE)
     @JoinColumn(name = "author_id")
     private User author;
 
     /** Thumbnail of the recipe. */
-    @Size(max = 64)
+    @Column
+    @Size(min = 3, max = 64)
     @NotNull
-    public String thumbnail;
+    private String thumbnail;
+
+    /** Ingredients of the recipe. */
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.REMOVE)
+    private Collection<PortionedIngredient> ingredients;
 
     /** Creation time of the entity. */
     @CreationTimestamp

261b512e77595673548f15cf6c3ada14a8b5e78e
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-06-24 15:13:39 +0200

    change repository to jpa repository

--- src/main/java/com/valentinusz/recipes/repositories/IngredientRepository.java
+++ src/main/java/com/valentinusz/recipes/repositories/IngredientRepository.java
@@ -1,6 +1,6 @@
 package com.valentinusz.recipes.repositories;
 
 import com.valentinusz.recipes.models.Ingredient;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IngredientRepository extends CrudRepository<Ingredient, Long> { }
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> { }

8abb889b63b261f9166a13bd44c49fe0d34c4769
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-07-03 19:46:05 +0200

    split seeder into multiple functions

--- src/main/java/com/valentinusz/recipes/database/DatabaseSeeder.java
+++ src/main/java/com/valentinusz/recipes/database/DatabaseSeeder.java
@@ -11,9 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.StreamSupport;
 
 /**
  * Class responsible for seeding the database with test data.
@@ -46,8 +46,8 @@ public class DatabaseSeeder implements CommandLineRunner {
     /**
      * Constructor.
      *
-     * @param faker Faker instance.
-     * @param userRepository UserRepository instance.
+     * @param faker           Faker instance.
+     * @param userRepository  UserRepository instance.
      * @param passwordEncoder BCryptPasswordEncoder instance.
      */
     public DatabaseSeeder(@Autowired Faker faker, @Autowired UserRepository userRepository, @Autowired BCryptPasswordEncoder passwordEncoder, @Autowired DietaryRestrictionRepository dietaryRestrictionRepository, @Autowired IngredientRepository ingredientRepository, @Autowired PortionedIngredientRepository portionedIngredientRepository, @Autowired RecipeRepository recipeRepository) {
@@ -67,83 +67,101 @@ public class DatabaseSeeder implements CommandLineRunner {
 
     /** Seeds the database. */
     public void seed() {
-        if (userRepository.count() == 0) {
-            List<DietaryRestriction> restrictions = StreamSupport.stream(dietaryRestrictionRepository.findAll().spliterator(), false).toList();
+        IngredientCategory[] ingredientCategories = IngredientCategory.values();
+        ServingSize[] servingSizes = ServingSize.values();
+        List<DietaryRestriction> restrictions = dietaryRestrictionRepository.findAll();
 
-            List<User> users = new LinkedList<>();
-            User admin = new User();
-            admin.setUserName("admin");
-            admin.setEmail("admin@recipes.com");
-            admin.setEmailVerifiedAt(Instant.now());
-            admin.setPassword(passwordEncoder.encode("adminpwd"));
-            admin.setIsAdmin(true);
+        Collection<User> users = seedUsers(restrictions);
+        userRepository.saveAll(users);
 
-            userRepository.save(admin);
-            users.add(admin);
+        Collection<Ingredient> ingredients = seedIngredients(ingredientCategories);
+        ingredientRepository.saveAll(ingredients);
 
-            for (int i = 0; i < 10; i++) {
-                User user = new User();
-                user.setUserName(faker.name().username());
-                user.setEmail(faker.internet().emailAddress());
-                user.setPassword(passwordEncoder.encode("password"));
-
-                // 50% to have a dietary restriction
-                if (faker.bool().bool()) {
-                    int start = faker.random().nextInt(0, restrictions.size() - 1);
-                    // approx. 25% to have 2
-                    int length = (start != (restrictions.size() - 1) && faker.random().nextInt(0, 3) == 0 ? 2 : 1);
-
-                    user.setDietaryRestrictions(restrictions.subList(start, start + length));
-                }
-
-                userRepository.save(user);
-                users.add(user);
-            }
-
-
-            var categories = IngredientCategory.values();
-            for (int i = 0; i < 30; i++) {
-                Ingredient ingredient = new Ingredient();
-                ingredient.setName(faker.food().ingredient());
-                ingredient.setEnergy(faker.random().nextInt(0, 2000));
-                ingredient.setCategory(categories[faker.random().nextInt(0, categories.length - 1 )]);
-
-                ingredientRepository.save(ingredient);
-            }
-
-            var servingSizes = ServingSize.values();
-            for (User user : users) {
-                // for each user generate 0-3 recipes
-                for (int i = 0; i < faker.random().nextInt(0, 3); i++) {
-                    List<Ingredient> ingredients = ingredientRepository.findAll();
-                    Recipe recipe = new Recipe();
-                    recipe.setName(faker.food().dish());
-                    recipe.setPreparationTime(faker.random().nextInt(0, 30));
-                    recipe.setCookingTime(faker.random().nextInt(0, 90));
-                    recipe.setInstructions(String.join(" ", faker.lorem().paragraphs(faker.random().nextInt(2, 4))));
-                    recipe.setAuthor(user);
-
-                    recipeRepository.save(recipe);
-
-                    // generate 3-6 ingredients with portions
-                    List<PortionedIngredient> portionedIngredients = new LinkedList<>();
-                    for (int j = 0; j < faker.random().nextInt(3, 6); j++) {
-                        if (!ingredients.isEmpty()) {
-                            PortionedIngredient portionedIngredient = new PortionedIngredient();
-                            portionedIngredient.setRecipe(recipe);
-                            portionedIngredient.setIngredient(ingredients.remove(faker.number().numberBetween(0, ingredients.size() -1)));
-
-                            portionedIngredient.setServingSize(servingSizes[faker.random().nextInt(0, servingSizes.length - 1 )]);
-                            portionedIngredient.setAmount(faker.number().randomDouble(2, 1, 100));
-
-                            portionedIngredientRepository.save(portionedIngredient);
-                            portionedIngredients.add(portionedIngredient);
-                        }
-                    }
-
-                    recipe.setIngredients(portionedIngredients);
-                }
-            }
-        }
+//        int recipeCount = users.size() * 3;
+//
+//            for (int i = 0; i < faker.random().nextInt(0, 3); i++) {
+//                List<Ingredient> ingredients = ingredientRepository.findAll();
+//
+//                recipeRepository.save(recipe);
+//
+//                // generate 3-6 ingredients with portions
+//
+//                recipe.setIngredients(portionedIngredients);
+//            }
     }
+
+    private Collection<User> seedUsers(List<DietaryRestriction> dietaryRestrictions) {
+        List<User> users = new LinkedList<>();
+        User admin = new User();
+        admin.setUserName("admin");
+        admin.setEmail("admin@recipes.com");
+        admin.setEmailVerifiedAt(Instant.now());
+        admin.setPassword(passwordEncoder.encode("adminpwd"));
+        admin.setIsAdmin(true);
+
+        userRepository.save(admin);
+        users.add(admin);
+
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setUserName(faker.name().username());
+            user.setEmail(faker.internet().emailAddress());
+            user.setPassword(passwordEncoder.encode("password"));
+
+            // 50% to have a dietary restriction
+            if (faker.bool().bool()) {
+                int start = faker.random().nextInt(dietaryRestrictions.size() - 1);
+                // approx. 25% to have 2
+                int length = (start != (dietaryRestrictions.size() - 1) && faker.random().nextInt(3) == 0 ? 2 : 1);
+
+                user.setDietaryRestrictions(dietaryRestrictions.subList(start, start + length));
+            }
+
+            users.add(user);
+        }
+
+        return users;
+    }
+
+    private Collection<Ingredient> seedIngredients(IngredientCategory[] categories) {
+        List<Ingredient> ingredients = new LinkedList<>();
+        for (int i = 0; i < 30; i++) {
+            Ingredient ingredient = new Ingredient();
+            ingredient.setName(faker.food().ingredient());
+            ingredient.setEnergy(faker.random().nextInt(0, 2000));
+            ingredient.setCategory(categories[faker.random().nextInt(0, categories.length - 1)]);
+            ingredients.add(ingredient);
+        }
+        return ingredients;
+    }
+
+//    private Collection<PortionedIngredient> seedPortionedIngredients(Collection<Ingredient> ingredients, Collection<Recipe> recipes) {
+//        List<PortionedIngredient> portionedIngredients = new LinkedList<>();
+//        // create 3-4 portioned version for each ingredient
+//        for (Ingredient ingredient: ingredients) {
+//            PortionedIngredient portionedIngredient = new PortionedIngredient();
+////            portionedIngredient.setRecipe(recipe);
+//            portionedIngredient.set
+//            portionedIngredient.setServingSize(servingSizes[faker.random().nextInt(0, servingSizes.length - 1)]);
+//                        portionedIngredient.setAmount(faker.number().randomDouble(2, 1, 100));
+//
+//                        portionedIngredientRepository.save(portionedIngredient);
+//                        portionedIngredients.add(portionedIngredient);
+//        }
+//
+//
+//        return portionedIngredients;
+//    }
+
+//    private Collection<Recipe> seedRecipe() {
+//        List<Recipe> recipes = new LinkedList<>();
+//        Recipe recipe = new Recipe();
+//        recipe.setName(faker.food().dish());
+//        recipe.setPreparationTime(faker.random().nextInt(0, 30));
+//        recipe.setCookingTime(faker.random().nextInt(0, 90));
+//        recipe.setInstructions(String.join(" ", faker.lorem().paragraphs(faker.random().nextInt(2, 4))));
+//        recipe.setAuthor(user);
+//        recipes.add(recipe);
+//        return recipes;
+//    }
 }

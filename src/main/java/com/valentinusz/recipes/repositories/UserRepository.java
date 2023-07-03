389fed93426716d8d5d686a5a635da49bff572b6
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-07-03 19:46:30 +0200

    switch to jpa repository interface

--- src/main/java/com/valentinusz/recipes/repositories/UserRepository.java
+++ src/main/java/com/valentinusz/recipes/repositories/UserRepository.java
@@ -1,14 +1,14 @@
 package com.valentinusz.recipes.repositories;
 
 import com.valentinusz.recipes.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 /** Repository for the user entity. */
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     /**
      * Finds the given user and loads their dietary restrictions.
      * @param id id of the user to fetch information of.

104e11a45efc078ac4919a5d667f0899b459684f
Valentinusz <bb20020402@gmail.com>
2023-07-05 20:34:58 +0200

    rework entity to use relationship instead of inheritance

--- src/main/java/com/valentinusz/recipes/models/PortionedIngredient.java
+++ src/main/java/com/valentinusz/recipes/models/PortionedIngredient.java
@@ -4,20 +4,35 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Class representing an ingredient with portions.
+ * @see Ingredient
+ */
 @Entity
 @Table(name = "portioned_ingredients")
 @Getter
 @Setter
-public class PortionedIngredient extends Ingredient {
+public class PortionedIngredient {
+    /** Id of the ingredient. **/
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /** Ingredient entity being portioned. */
+    @ManyToOne
+    @JoinColumn(name = "ingredient_id")
+    private Ingredient ingredient;
+
     /** Recipe the ingredient is used in with the given portions. */
     @ManyToOne
     @JoinColumn(name = "recipe_id")
     private Recipe recipe;
 
-    /** */
+    /** Serving size of the portion. */
     @Enumerated(value = EnumType.STRING)
     private ServingSize servingSize;
 
+    /** Amount of the portion. **/
     @Column(precision = 8)
     private Double amount;
 }

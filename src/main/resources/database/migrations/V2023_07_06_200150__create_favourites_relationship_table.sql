9a294f4821d22e172425a915985b926588fa4e7b
Valentinusz <bb20020402@gmail.com>
2023-07-06 21:32:35 +0200

    create junction table migration for favourites

---
+++ src/main/resources/database/migrations/V2023_07_06_200150__create_favourites_relationship_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE favourites (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    recipe_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX no_duplicate ON favourites(user_id, recipe_id)

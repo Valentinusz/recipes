317471f730522c75dbfd998cf2e421dadf454345
Valentinusz <bb20020402@gmail.com>
2023-07-07 14:17:28 +0200

    clean up migrations

--- src/main/resources/database/migrations/V2023_06_16_000000__create_users_table.sql
+++ src/main/resources/database/migrations/V2023_06_16_000000__create_users_table.sql
@@ -1,10 +1,11 @@
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
-    user_name VARCHAR(20) CHECK ( LENGTH(user_name) BETWEEN 4 AND 20 ) NOT NULL,
-    email VARCHAR(32) CHECK ( email LIKE '_%@_%.__%') UNIQUE NOT NULL,
+CREATE TABLE users
+(
+    id                INTEGER PRIMARY KEY,
+    user_name         VARCHAR(20) UNIQUE NOT NULL CHECK ( LENGTH(user_name) BETWEEN 4 AND 20 ),
+    email             VARCHAR(32) UNIQUE NOT NULL CHECK ( email LIKE '_%@_%.__%'),
     email_verified_at TIMESTAMP,
-    password VARCHAR(60) NOT NULL,
-    is_admin BIT DEFAULT 0 NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    password          VARCHAR(60)        NOT NULL,
+    is_admin          BIT DEFAULT 0      NOT NULL,
+    created_at        TIMESTAMP          NOT NULL,
+    updated_at        TIMESTAMP
 )
--- src/main/resources/database/migrations/V2023_06_19_201500__create_recipes_table.sql
+++ src/main/resources/database/migrations/V2023_06_19_201500__create_recipes_table.sql
@@ -1,13 +1,15 @@
-CREATE TABLE recipes (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(64) CHECK ( LENGTH(name) BETWEEN 2 AND 64) NOT NULL,
-    preparation_time INTEGER CHECK ( preparation_time >= 0 ) NOT NULL,
-    cooking_time INTEGER CHECK ( cooking_time >= 0 ) NOT NULL,
-    instructions TEXT NOT NULL,
-    author_id INTEGER NOT NULL,
-    thumbnail VARCHAR(64),
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+CREATE TABLE recipes
+(
+    id               INTEGER PRIMARY KEY,
+    name             VARCHAR(64) NOT NULL CHECK ( LENGTH(name) BETWEEN 2 AND 64 ),
+    preparation_time INTEGER     NOT NULL CHECK ( preparation_time >= 0 ),
+    cooking_time     INTEGER     NOT NULL CHECK ( cooking_time >= 0 ),
+    instructions     TEXT        NOT NULL,
+    author_id        INTEGER     NOT NULL,
+    thumbnail        VARCHAR(64) NOT NULL,
+    created_at       TIMESTAMP   NOT NULL,
+    updated_at       TIMESTAMP,
+    
     FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE
 )
 
